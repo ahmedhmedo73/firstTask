dc949d069f8a676bd9c3d7ed84fe500c878d9a91
ahmed <ahmedhmedo73@gmail.com>
2020-06-19 09:16:26 -0700

    Initial Commit

---
+++ fisrttask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fisrttask
+//
+//  Created by ahmed kamel on 10/27/1441 AH.
+//  Copyright © 1441 AH ahmed kamel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
